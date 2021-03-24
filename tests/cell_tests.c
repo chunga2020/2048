a64fbe1a32a8c4c0df045a10433ac4316be8fe70
Andrew Chung <andrewtc4@gmail.com>
2021-03-24 10:00:00 -0400

    tests/cell_tests.c: Augment creation test
    We need to make sure clean-up is tested as well as creation.

--- tests/cell_tests.c
+++ tests/cell_tests.c
@@ -1,16 +1,19 @@
 #include "cell.h"
 #include "minunit.h"
 
-char *test_Cell_create() {
+char *test_Cell_create_and_destroy() {
     Cell *c = Cell_create();
     mu_assert(c != NULL, "Failed to create Cell!");
+    Cell_destroy(c);
+    c = NULL;
+    mu_assert(c == NULL, "Failed to clean up!");
     return NULL;
 }
 
 char *all_tests() {
     mu_suite_start();
 
-    mu_run_test(test_Cell_create);
+    mu_run_test(test_Cell_create_and_destroy);
 
     return NULL;
 }
